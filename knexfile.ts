8762dfd5ef6da0ceb149a55a4bb3cca5ee91677d
Dárley Raffael <darleyraffael@gmail.com>
2023-09-14 10:16:24 -0300

    ✨feat: add knex config setup initial

---
+++ knexfile.ts
@@ -0,0 +1,15 @@
+import { Knex } from "knex";
+import { env } from "./src/env";
+
+const knexConfig: Knex.Config = {
+  client: "sqlite3",
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./src/database/migrations",
+  },
+};
+
+export default knexConfig;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import { transactionsRoutes } from "./routes/transactions";
+
+const app = fastify();
+
+app.register(transactionsRoutes, {
+  prefix: "/transactions",
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("🚀 Server started on port http://localhost:3333");
+  });
