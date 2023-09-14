8d3f357ec6b82357ca191af55c5239324fa4be66
Dárley Raffael <darleyraffael@gmail.com>
2023-09-14 16:56:49 -0300

    💡chore: modifies the env instance

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import fastify from "fastify";
 import { transactionsRoutes } from "./routes/transactions";
 import cookie from "@fastify/cookie";
-
+import { env } from "./env";
 
 const app = fastify();
 
-app.register(cookie)
+app.register(cookie);
 app.register(transactionsRoutes, {
   prefix: "/transactions",
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("🚀 Server started on port http://localhost:3333");
